489f158c394a46414949aab130f6d6c1f3c1ddc4
Md Farhan <farhansiddiquii438@gmail.com>
2025-07-01 00:59:51 +0530

    modified

--- MostRepeatedCharIndex/src/Example.java
+++ MostRepeatedCharIndex/src/Example.java
@@ -1,63 +1,54 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Example {
 
 	public static void main(String[] args) {
-	
+
 		String str = "success";
-		
+
 		findMostRepeatedIndex(str);
 	}
 
 	private static void findMostRepeatedIndex(String str) {
-		
-		
-		char[] arr =str.toCharArray();
-		
-	  char	mostRepeated =arr[0];
-	  int maxCount=0;
-	  boolean[] visited = new boolean[arr.length];
-	  
-	  for(int i=0;i<arr.length;i++) {
-		  if(visited[i]) {
-			  continue;
-		  }
-		  
-		  int count=1;
-		  
-		  for(int j=i+1;j<arr.length;j++) {
-			  if(arr[i]==arr[j]) {
-				 count++;
-				  visited[j]=true;
-				  
-			  }
-		  }
-		  
-		  
-		  if(count>maxCount) {
-			  maxCount=count;
-			  mostRepeated=arr[i];
-		  }
-	  }
-	  
-	  
-	  
-	 List<Integer> indices = new ArrayList<>();
-		
-	 for(int i=0;i<arr.length;i++) {
-		 if(arr[i]==mostRepeated) {
-			 indices.add(i);
-		 }
-	 }
-	 System.out.println("Most repeated char is " + mostRepeated + ": ( occured " + maxCount + " times )");
 
-	 System.out.print("At Index : ");
-	 
-	 for(int index:indices) {
-		 System.out.print(index+" ");
-	 }
-	 
+		char[] arr = str.toCharArray();
+
+		char mostRepeated = arr[0];
+		int maxCount = 0;
+		boolean[] visited = new boolean[arr.length];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (visited[i]) {
+				continue;
+			}
+
+			int count = 1;
+
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					count++;
+					visited[j] = true;
+
+				}
+			}
+
+			if (count > maxCount) {
+				maxCount = count;
+				mostRepeated = arr[i];
+			}
+		}
+
+		if (maxCount > 1) {
+			System.out.print("Most repeated char is '" + mostRepeated + "': ( occured " + maxCount + " times )");
+			System.out.print(" At Index : ");
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] == mostRepeated) {
+					System.out.print(i + " ");
+				}
+			}
+
+		} else {
+			System.out.println("No Char is repeated");
+		}
+
 	}
 
 }
